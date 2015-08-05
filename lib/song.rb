499328035b495c1e1a615d8dc8f33c80b3d8942f
Drew Price <drewprice@users.noreply.github.com>
2015-08-05 14:03:59 -0400

    Remove absolute path

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   attr_accessor :title, :artist
 
   def serialize
-    out = Tempfile.open([sanitize_title, '.txt'], '/Users/drew/oo-data-normalization-web-0715/tmp')
+    out = Tempfile.open([sanitize_title, '.txt'], './tmp')
     out.write("#{artist.name} - #{title}")
     out.rewind
     out.close

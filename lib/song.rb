b8f5158eb9d83a34e5ec6ed35e81c491813a1415
Drew Price <drewprice@users.noreply.github.com>
2015-08-05 13:53:20 -0400

    Add solution

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 class Song
-  # code goes here
+  attr_accessor :title, :artist
+
+  def serialize
+    out = Tempfile.open([sanitize_title, '.txt'], '/Users/drew/oo-data-normalization-web-0715/tmp')
+    out.write("#{artist.name} - #{title}")
+    out.rewind
+    out.close
+  end
+
+  private
+
+  def sanitize_title
+    @title.downcase.gsub(/ /, '_')
+  end
 end
